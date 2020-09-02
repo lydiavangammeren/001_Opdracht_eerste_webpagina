bcbc20dc9fcb69825d7b8b8a8ccd0be53a91c107
Lydia <67099359+lydiavangammeren@users.noreply.github.com>
2020-09-02 19:24:12 +0200

    Add files via upload

---
+++ Module3_Level14/3_008_Opdracht_For_Loops/even-odd-reporter.js
@@ -0,0 +1,5 @@
+for (i = 0; i<= 20; i++) {
+    if (i%2)
+    console.log(`${i} is uneven`)
+    else {console.log(`${i} is even`)}
+}
---
+++ Module3_Level14/3_008_Opdracht_For_Loops/grade-assigner.js
@@ -0,0 +1,19 @@
+function assignGrade(score) {
+    if (score > 90) {
+        return 'A';
+    } else if (score > 80) {
+        return 'B';
+    } else if (score > 70) {
+        return 'C';
+    } else if (score > 65) {
+        return 'D';
+    } else {
+        return 'F';
+    }
+}
+
+
+for (i=60; i<=100; i++) {
+    assignGrade(i);
+    console.log(`Voor ${i} punten krijg je een ${assignGrade(i)}`)
+}
